1c737cae0dd5a9aea1d291988a9dcfacfd7df7e3
joachim-gabin <ejoachimgabin@gaming.tech>
2023-01-26 10:00:51 +0100

    Changement répertoir + test Unitaire
    Changement répertoir + test Unitaire

---
+++ projet_test/Test_Unitaire_Projet_C_Sharp/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Test_Unitaire_Projet_C_Sharp
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
